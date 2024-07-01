92e77fb4d1e5535a6a87490e1c3297294f83a32c
Derek Hemingson <shadetheartist@gmail.com>
2024-06-30 21:23:05 -0700

    benchmark different games instead of same seed each time

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,11 +1,12 @@
 use rand_chacha::rand_core::SeedableRng;
 use acquire::{Acquire, Options};
 use rand::seq::SliceRandom;
+use rand::{RngCore, thread_rng};
 
 fn main() {
     for n in 0..10_000 {
-        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(n);
-        let mut game = Acquire::new(rand_chacha::ChaCha8Rng::seed_from_u64(n), &Options::default());
+        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(thread_rng().next_u64());
+        let mut game = Acquire::new(rand_chacha::ChaCha8Rng::seed_from_u64(thread_rng().next_u64()), &Options::default());
 
         loop {
             if game.is_terminated() {
